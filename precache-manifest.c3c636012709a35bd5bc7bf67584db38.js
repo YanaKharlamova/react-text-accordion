1e746ad510ddd4bacf11d1fc3b32fda5a9f16c25
Yana Kh <yanakarlamv@gmail.com>
2022-05-10 15:34:38 +0300

    Updates

---
+++ precache-manifest.c3c636012709a35bd5bc7bf67584db38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae2ed8194ad5b03b8f6456d2665f485",
+    "url": "/react-text-accordion/index.html"
+  },
+  {
+    "revision": "199b09d0a0d102c14688",
+    "url": "/react-text-accordion/static/css/main.ca3b564e.chunk.css"
+  },
+  {
+    "revision": "2a5d63bae00c3ab1a7e5",
+    "url": "/react-text-accordion/static/js/2.16eb259b.chunk.js"
+  },
+  {
+    "revision": "199b09d0a0d102c14688",
+    "url": "/react-text-accordion/static/js/main.078738b3.chunk.js"
+  },
+  {
+    "revision": "47c5af18e1f10145fdf7",
+    "url": "/react-text-accordion/static/js/runtime~main.d6130da6.js"
+  }
+]);
